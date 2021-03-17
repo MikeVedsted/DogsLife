1dbbe39a04fc71455bc89c48f485cb447d568312
Mike Vedsted <mikevedsted@gmail.com>
2021-03-17 12:31:45 +0200

    Add unknownEndpoint middleware

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 import cors from 'cors'
 import compression from 'compression'
 import helmet from 'helmet'
 
 const app = express()
 
+app.set('port', process.env.PORT || 5000)
 app.use(cors())
 app.use(express.json())
 app.use(compression())
@@ -13,5 +14,13 @@ app.use(helmet())
 app.get('/api/bow', (_req, res) => {
   res.send('wow')
 })
+
+app.set('port', process.env.PORT || 5000)
+
+const unknownEndpoint = (_request: Request, response: Response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 export default app
