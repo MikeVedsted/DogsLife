9550b7c83eb96b52d850f33f47db0d8e02498652
Mike Vedsted <mikevedsted@gmail.com>
2021-03-17 11:26:33 +0200

    lint index with new configs

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-import express from "express";
-import cors from "cors";
+import express from "express"
+import cors from "cors"
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 
-app.get("/api/ping", (_req, res) => {
-  res.send("pong");
-});
+app.get("/api/bow", (_req, res) => {
+  res.send("wow")
+})
 
-const PORT = 8080;
+const PORT = 8080
 
 app.listen(PORT, () => {
-  console.log(`Server ready and running on port ${PORT}`);
-});
+  console.log(`Server ready and running on port ${PORT}`)
+})
