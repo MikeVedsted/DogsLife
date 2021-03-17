5c1b1217c232998c56d0308fbe672220ad382b19
Mike Vedsted <mikevedsted@gmail.com>
2021-03-17 14:27:19 +0200

    Configure DB

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,35 @@
 import express, { Request, Response } from 'express'
-import cors from 'cors'
 import compression from 'compression'
+import mongoose from 'mongoose'
 import helmet from 'helmet'
+import cors from 'cors'
+
+import config from '../util/config'
 
 const app = express()
 
-app.set('port', process.env.PORT || 5000)
-app.use(cors())
+const MONGODB_URI = config.MONGODB_URI
+const PORT = config.PORT
+
+app.set('port', PORT || 5000)
 app.use(express.json())
 app.use(compression())
 app.use(helmet())
+app.use(cors())
+
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error: Error) => {
+    console.log('error connection to MongoDB:', error.message)
+  })
 
 app.get('/api/bow', (_req, res) => {
   res.send('wow')
