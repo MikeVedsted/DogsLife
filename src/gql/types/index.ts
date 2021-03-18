8b719d53c17ee42742e7336dcc10705d405a6f05
Mike Vedsted <mikevedsted@gmail.com>
2021-03-18 15:53:53 +0200

    Add createUser mutation

--- src/gql/types/index.ts
+++ src/gql/types/index.ts
@@ -1,17 +1,33 @@
 import { gql } from 'apollo-server-express'
 
 const typeDefs = gql`
-  type Dog {
-    name: String!
-  }
+        type User {
+          name: String!
+          email: String!
+          passwordHash: String!
+          id: ID!
+        }
 
-  type Query {
-    allDogs: [Dog]
-  }
+        type Token {
+          value: String!
+        }
 
-  type Mutation {
-    addDog(name: String!): Dog
-  }
+        type Dog {
+          name: String!
+          id: ID!
+        }
+
+        type Query {
+          allDogs: [Dog]
+          allUsers: [User]
+          me: User
+        }
+
+        type Mutation {
+          addDog(name: String!): Dog
+          createUser(name: String!, email: String!, password: String!): User
+          login(email: String!, password: String!): Token
+        }
 `
 
 export default typeDefs
