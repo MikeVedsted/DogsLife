5b54643640085e8ed7de833b37af4cd374c29eee
Mike Vedsted <mikevedsted@gmail.com>
2021-03-22 14:17:17 +0200

    Integrate calendar nav in calendar

--- client/src/components/Calendar/index.tsx
+++ client/src/components/Calendar/index.tsx
@@ -1,11 +1,20 @@
+import React, { useState } from 'react'
 import Header from './Header'
 import Body from './Body'
+import CalendarNavigation from './CalendarNavigation'
 
 const Calendar = () => {
+  const [month, setMonth] = useState<number>(new Date().getMonth())
+  
+  const handleChange = (month: number) => {
+    setMonth(month)
+  }
+
   return (
     <div style={{ maxWidth: '100vw', maxHeight: '100vh' }}>
       <table style={{ width: '700px' }}>
         <Header />
+        <CalendarNavigation month={month} handleChange={handleChange} />
         <Body />
       </table>
     </div>
