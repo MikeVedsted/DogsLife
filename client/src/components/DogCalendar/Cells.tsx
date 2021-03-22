6b036292d472f6a7d46faddf34f52ab759ed77f9
Mike Vedsted <mikevedsted@gmail.com>
2021-03-22 11:11:04 +0200

    Add loop to populate cells array with initial padding

--- client/src/components/DogCalendar/Cells.tsx
+++ client/src/components/DogCalendar/Cells.tsx
@@ -6,8 +6,14 @@ const Cells = () => {
     0
   ).getDate()
   const firstDayInCurrentMonth = now.getDay()
-
+  const initialPadding =
+    firstDayInCurrentMonth === 0 ? 7 : firstDayInCurrentMonth
+  
   let cells = []
+
+  for (let i = 1; i < initialPadding; i++) {
+    cells.push(<td style={{ border: '1px solid white' }}>-</td>)
+  }
 
   for (let i = 1; i <= daysInCurrentMonth; i++) {
     cells.push(<td style={{ border: '1px solid white' }}>{i}</td>)
