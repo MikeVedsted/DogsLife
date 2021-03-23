9a93a2bb41515929da849c4fa7a572502da782d1
Mike Vedsted <mikevedsted@gmail.com>
2021-03-23 12:15:40 +0200

    Add login view

--- client/src/Routes.tsx
+++ client/src/Routes.tsx
@@ -1,14 +1,16 @@
 import { Switch, Route } from 'react-router-dom'
 
 import Dog from './components/Views/Dog'
+import Login from './components/Views/Login'
 import Calendar from './components/Calendar'
 import DogOverview from './components/Views/Dogs'
 
 const Routes = () => (
   <Switch>
-    <Route exact path="/dog/:dogId" component={Dog} />
-    <Route exact path="/dog/:dogId/calendar" component={Calendar} />
-    <Route path="/" component={DogOverview} />
+    <Route exact path='/dog/:dogId' component={Dog} />
+    <Route exact path='/dog/:dogId/calendar' component={Calendar} />
+    <Route exact path='/login' component={Login} />
+    <Route path='/' component={DogOverview} />
   </Switch>
 )
 
