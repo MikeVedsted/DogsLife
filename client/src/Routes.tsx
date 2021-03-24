39c219eeaa3a21088beb456c58d44635a8eaf601
Mike Vedsted <mikevedsted@gmail.com>
2021-03-24 13:10:49 +0200

    Unprotect login route

--- client/src/Routes.tsx
+++ client/src/Routes.tsx
@@ -12,7 +12,7 @@ import { ProtectedRouteProps } from './types'
 
 const ProtectedRoute = ({ component: Component, ...rest }: ProtectedRouteProps) => {
   const { loading, called, data } = useQuery(CHECK_AUTH, { fetchPolicy: 'network-only' })
-  let isAuth: string | null = called && !loading && data.me ? data.me.id : null
+  let isAuth: string | null = called && !loading && data && data.me ? data.me.id : null
 
   if (loading) {
     return <p>Loading...</p>
@@ -31,7 +31,7 @@ const Routes = () => (
     <ProtectedRoute exact path='/dogs' component={Dogs} />
     <ProtectedRoute exact path='/dog/:dogId' component={Dog} />
     <ProtectedRoute exact path='/dog/:dogId/calendar' component={Calendar} />
-    <ProtectedRoute exact path='/login' component={Login} />
+    <Route exact path='/login' component={Login} />
     <Route path='/' component={Home} />
   </Switch>
 )
