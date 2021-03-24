0b65300c55e7648cf0490eccec9c45f2df2b7b32
Mike Vedsted <mikevedsted@gmail.com>
2021-03-24 08:27:30 +0200

    Wrap Router in AuthContext

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,17 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
-import {
-  ApolloClient,
-  ApolloProvider,
-  HttpLink,
-  InMemoryCache,
-  split
-} from '@apollo/client'
+import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache, split } from '@apollo/client'
 // import { WebSocketLink } from '@apollo/client/link/ws'
 import { setContext } from '@apollo/client/link/context'
 import { getMainDefinition } from '@apollo/client/utilities'
 import AuthStorage from './util/AuthStorage'
+import AuthContext from './contexts/AuthContext'
 
 import './index.css'
 import App from './App'
@@ -31,8 +26,6 @@ const authLink = setContext((_, { headers }) => {
   }
 })
 
-
-
 // const wsLink = new WebSocketLink({
 //   uri: `ws://localhost:8080/graphql`,
 //   options: {
@@ -43,10 +36,7 @@ const authLink = setContext((_, { headers }) => {
 const splitLink = split(
   ({ query }) => {
     const definition = getMainDefinition(query)
-    return (
-      definition.kind === 'OperationDefinition' &&
-      definition.operation === 'subscription'
-    )
+    return definition.kind === 'OperationDefinition' && definition.operation === 'subscription'
   },
   // wsLink,
   authLink.concat(httpLink)
@@ -61,9 +51,11 @@ const client = new ApolloClient({
 ReactDOM.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <Router>
-        <App />
-      </Router>
+      <AuthContext.Provider value={{ token: AuthStorage.getAccessToken() }}>
+        <Router>
+          <App />
+        </Router>
+      </AuthContext.Provider>
     </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
