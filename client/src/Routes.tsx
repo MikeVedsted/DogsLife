5a5304a4fb1d6d75e8d0d633da96d59d3bee3d4f
Mike Vedsted <mikevedsted@gmail.com>
2021-03-24 10:13:02 +0200

    Protect routes other than home

--- client/src/Routes.tsx
+++ client/src/Routes.tsx
@@ -2,9 +2,11 @@ import { Switch, Route, Redirect } from 'react-router-dom'
 import { useQuery } from '@apollo/client'
 
 import Dog from './components/Views/Dog'
+import Dogs from './components/Views/Dogs'
+import Home from './components/Views/Home'
 import Login from './components/Views/Login'
 import Calendar from './components/Calendar'
-import DogOverview from './components/Views/Dogs'
+
 import { CHECK_AUTH } from './gql/queries'
 import { ProtectedRouteProps } from './types'
 
@@ -19,19 +21,18 @@ const ProtectedRoute = ({ component: Component, ...rest }: ProtectedRouteProps) 
   return (
     <Route
       {...rest}
-      render={(props) =>
-        isAuth ? <Component {...props} /> : <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
-      }
+      render={(props) => (isAuth ? <Component {...props} /> : <Redirect to={{ pathname: '/login' }} />)}
     />
   )
 }
 
 const Routes = () => (
   <Switch>
+    <ProtectedRoute exact path='/dogs' component={Dogs} />
     <ProtectedRoute exact path='/dog/:dogId' component={Dog} />
-    <Route exact path='/dog/:dogId/calendar' component={Calendar} />
-    <Route exact path='/login' component={Login} />
-    <Route path='/' component={DogOverview} />
+    <ProtectedRoute exact path='/dog/:dogId/calendar' component={Calendar} />
+    <ProtectedRoute exact path='/login' component={Login} />
+    <Route path='/' component={Home} />
   </Switch>
 )
 
