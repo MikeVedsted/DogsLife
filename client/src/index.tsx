1cb6366c10805c6418c159f2196b664e41bf8fc4
Mike Vedsted <mikevedsted@gmail.com>
2021-03-24 13:59:28 +0200

    Remove webvitals

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -10,7 +10,6 @@ import AuthContext from './contexts/AuthContext'
 
 import './index.css'
 import App from './App'
-import reportWebVitals from './reportWebVitals'
 
 const httpLink = new HttpLink({
   uri: '/graphql'
@@ -60,8 +59,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
