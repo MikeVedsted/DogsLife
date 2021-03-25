73ce1fb9e1dc6320f9af704d8269f11361652423
Mike Vedsted <mikevedsted@gmail.com>
2021-03-25 08:49:47 +0200

    Add dogs to user schema

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,9 +1,11 @@
 import { Document, Schema, model } from 'mongoose'
+import { DogDocument } from './Dog'
 
 export type UserDocument = Document & {
   name: string
   email: string
   passwordHash: string
+  dogs: Array<DogDocument>
 }
 
 const schema = new Schema({
@@ -18,7 +20,13 @@ const schema = new Schema({
   },
   passwordHash: {
     type: String
-  }
+  },
+  dogs: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Dog'
+    }
+  ]
 })
 
 export default model<UserDocument>('User', schema)
