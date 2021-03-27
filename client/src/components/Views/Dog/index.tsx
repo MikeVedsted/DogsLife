0bce35b3faa96eda0f00bd358aeb02d04f1eaa26
Mike Vedsted <mikevedsted@gmail.com>
2021-03-27 20:45:08 +0200

    Add dob display to Dog view

--- client/src/components/Views/Dog/index.tsx
+++ client/src/components/Views/Dog/index.tsx
@@ -1,14 +1,14 @@
 import { Link, useParams } from 'react-router-dom'
 import { useApolloClient } from '@apollo/client'
 
-import { ALL_DOGS } from '../../../gql/queries'
-import { AllDogsCache, Dog } from '../../../types'
+import { MY_DOGS } from '../../../gql/queries'
+import { MyDogsCache, Dog } from '../../../types'
 
 const DogView = () => {
   const { dogId } = useParams<{ dogId: string }>()
   const client = useApolloClient()
 
-  const cachedData: AllDogsCache | null = client.readQuery({ query: ALL_DOGS })
+  const cachedData: MyDogsCache | null = client.readQuery({ query: MY_DOGS })
 
   if (cachedData === null) {
     return (
@@ -18,13 +18,15 @@ const DogView = () => {
       </>
     )
   }
-
-  const currentDog: Dog = cachedData.allDogs.filter((dog: Dog) => dog.id === dogId)[0]
-
+  
+  const currentDog: Dog = cachedData.dogs.filter((dog: Dog) => dog.id === dogId)[0]
+  const dob = new Date(currentDog.dob).toDateString().substring(4)
+  
   return (
     <div>
       <h2>{currentDog.name}</h2>
       <h2>(id: {currentDog.id})</h2>
+      <p>Birthday: {dob}</p>
       <Link to={`/dog/${dogId}/calendar`}>go to calendar</Link>
     </div>
   )
