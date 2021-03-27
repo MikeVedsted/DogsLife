686c8190b065d84efe44e8407b4d13fdeecf6736
Mike Vedsted <mikevedsted@gmail.com>
2021-03-27 20:42:52 +0200

    Add custom scalar Date resolver

--- src/gql/resolvers/index.ts
+++ src/gql/resolvers/index.ts
@@ -2,7 +2,8 @@ import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import { ApolloError, UserInputError } from 'apollo-server-errors'
 import { PubSub } from 'apollo-server-express'
-
+import { GraphQLScalarType, Kind } from 'graphql'
+import { isValid, isDate } from 'date-fns'
 import config from '../../../util/config'
 import Dog, { DogDocument } from '../../models/Dog'
 import User, { UserDocument } from '../../models/user'
@@ -13,7 +14,53 @@ type Token = {
   value: string
 }
 
+const isValidDateTime = (value: any) => {
+  const isSerializable =
+    isDate(value) || typeof value === 'string' || typeof value === 'number'
+
+  return isSerializable ? isValid(new Date(value)) : false
+}
+
+const dateScalar = new GraphQLScalarType({
+  name: 'Date',
+  description: 'Date custom scalar type',
+  serialize(value) {
+    if (isValidDateTime(value)) {
+      return new Date(value).toISOString()
+    }
+
+    throw new TypeError(
+      `DateTime can not be serialized from ${JSON.stringify(value)}`
+    )
+  },
+  parseValue(value) {
+    if (isValidDateTime(value)) {
+      return new Date(value)
+    }
+
+    throw new TypeError(
+      `DateTime can not be parsed from ${JSON.stringify(value)}`
+    )
+  },
+  parseLiteral(ast) {
+    if (ast.kind !== Kind.STRING) {
+      throw new TypeError('DateTime cannot represent non string type')
+    }
+
+    const { value } = ast
+
+    if (isValidDateTime(value)) {
+      return new Date(value)
+    }
+
+    throw new TypeError(
+      `DateTime can not be parsed from ${JSON.stringify(value)}`
+    )
+  }
+})
+
 const resolvers = {
+  Date: dateScalar,
   Query: {
     allDogs: () => {
       return Dog.find({})
@@ -39,12 +86,13 @@ const resolvers = {
       if (args && args.id) {
         return await Dog.findById(args.id)
       }
+
       const user = await User.findById(context.currentUser._id).populate('dogs')
 
       if (!user) {
         throw new ApolloError('Something went wrong.. Please try again.', '500')
       }
-      return null
+      return [...user.dogs]
     }
   },
 
