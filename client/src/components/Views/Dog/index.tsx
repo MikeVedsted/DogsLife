9d9b2a9447eda1bc3820fdf82db5ffacb5721eab
Mike Vedsted <mikevedsted@gmail.com>
2021-03-28 20:49:54 +0300

    Add activity inpu

--- client/src/components/Views/Dog/index.tsx
+++ client/src/components/Views/Dog/index.tsx
@@ -3,10 +3,13 @@ import { useApolloClient } from '@apollo/client'
 
 import { MY_DOGS } from '../../../gql/queries'
 import { MyDogsCache, Dog } from '../../../types'
+import useInput from '../../../hooks/useInput'
+import { React } from '@ungap/global-this'
 
 const DogView = () => {
   const { dogId } = useParams<{ dogId: string }>()
   const client = useApolloClient()
+  const activityTime = useInput('number')
 
   const cachedData: MyDogsCache | null = client.readQuery({ query: MY_DOGS })
 
@@ -22,12 +25,22 @@ const DogView = () => {
   const currentDog: Dog = cachedData.dogs.filter((dog: Dog) => dog.id === dogId)[0]
   const dob = new Date(currentDog.dob).toDateString().substring(4)
   
+  const submitActivity = (e: React.SyntheticEvent) => {
+    e.preventDefault()
+    console.log(activityTime.value)
+  }
+
   return (
     <div>
       <h2>{currentDog.name}</h2>
       <h2>(id: {currentDog.id})</h2>
       <p>Birthday: {dob}</p>
       <Link to={`/dog/${dogId}/calendar`}>go to calendar</Link>
+      <p>Add activity:</p>
+      <form onSubmit={submitActivity}>
+        <input {...activityTime} />
+        <button>Add</button>
+      </form>
     </div>
   )
 }
